75853b67a671b9d5178ba40b0e313694ec1e44e6
adnofal <adam.nofal9@hotmail.com>
2022-09-24 18:09:42 +0300

    third question

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-set1 = {10, 20, 30, 40, 50}
+tuple1 = (10, 20, 30, 40, 50)
 
-set2 = {30, 40, 50, 60, 70}
+tuple2 = tuple(reversed(tuple1))
 
-result = set1.intersection(set2)
-
-print(result)
+print(tuple2)
